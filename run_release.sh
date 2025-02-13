6c981727d2129e3784f40654801e20023eabb429
5er9e1 <5er9e1@github.com>
2025-02-13 12:30:40 +0900

    add workflow

---
+++ run_release.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PRODUCT_VERSION="${PRODUCT_VERSION:-unknown_version}"
+PRODUCT_ENVIRONMENT="${PRODUCT_ENVIRONMENT:-unknown_environment}"
+
+echo "The new version ${PRODUCT_VERSION} of my product is released at ${PRODUCT_ENVIRONMENT}"
